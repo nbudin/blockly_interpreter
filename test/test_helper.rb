ccba5d9751406280b1e21022a50775e39bde2836
Stuart Garner <sgarner@patientslikeme.com>
2017-10-18 15:58:38 -0400

    Renaming test_helper.rb to blockly_interpreter_test_helper.rb so that it doesn't conflict with any test_helper files in the apps using this gem

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,6 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'blockly_interpreter'
-require 'blockly_interpreter/test_helper'
+require 'blockly_interpreter/blockly_interpreter_test_helper'
 
 require 'minitest/spec'
 require 'minitest/reporters'
